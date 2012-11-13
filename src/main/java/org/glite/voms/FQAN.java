ef085fd027f4c6dc2ff5cd56325aea66243b227b
Andrea Ceccanti <andrea.ceccanti@cnaf.infn.it>
2012-11-13 09:30:37 +0100

    Added license header to all source files. Prepared pom.xml for Maven Central release.

--- src/main/java/org/glite/voms/FQAN.java
+++ src/main/java/org/glite/voms/FQAN.java
@@ -20,12 +20,6 @@
  * follows.
  *
  *********************************************************************/
-/*
- * Copyright (c) Members of the EGEE Collaboration. 2004.
- * See http://eu-egee.org/partners/ for details on the copyright holders.
- * For license conditions see the license file or http://eu-egee.org/license.html
- */
-
 package org.glite.voms;
 
 
