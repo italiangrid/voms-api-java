0bcda617b14b9c409830f7e21fd5668fa4f54c80
Daniele Andreotti <daniele.andreotti@cnaf.infn.it>
2012-12-07 11:12:48 +0100

    Fixed a bug related to the --vomses option. Returned a List able to support the add method.

--- src/main/java/org/italiangrid/voms/request/impl/BaseVOMSESLookupStrategy.java
+++ src/main/java/org/italiangrid/voms/request/impl/BaseVOMSESLookupStrategy.java
@@ -23,34 +23,35 @@ import java.util.List;
 import org.italiangrid.voms.request.VOMSESLookupStrategy;
 
 /**
- * A base VOMSES lookup strategy which just run a existence check on a given list of paths.
+ * A base VOMSES lookup strategy which just run a existence check on a given
+ * list of paths.
  * 
  * @author Andrea Ceccanti
- *
+ * 
  */
 public class BaseVOMSESLookupStrategy implements VOMSESLookupStrategy {
 
-	private String[] checkedPaths;
-	
+	private final String[] checkedPaths;
+
 	public BaseVOMSESLookupStrategy(String[] checkedPaths) {
 		this.checkedPaths = checkedPaths;
 	}
-	
+
 	public List<File> lookupVomsesInfo() {
-		
+
 		List<File> vomsesPaths = new ArrayList<File>();
-		
-		for (String p: checkedPaths){
+
+		for (String p : checkedPaths) {
 			File f = new File(p);
 			if (f.exists())
 				vomsesPaths.add(f);
 		}
-			
+
 		return vomsesPaths;
 	}
 
 	public List<String> searchedPaths() {
-		return Arrays.asList(checkedPaths);
+		return new ArrayList<String>(Arrays.asList(checkedPaths));
 	}
 
 }
