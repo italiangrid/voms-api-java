f4a23f9d69e9f595aed2a821a5056881cd9eeae6
Daniele Andreotti <daniele.andreotti@cnaf.infn.it>
2012-12-07 12:36:17 +0100

    Added new location for looking up vomses files ($HOME/.voms)

--- src/main/java/org/italiangrid/voms/request/impl/DefaultVOMSESLookupStrategy.java
+++ src/main/java/org/italiangrid/voms/request/impl/DefaultVOMSESLookupStrategy.java
@@ -15,27 +15,27 @@
  */
 package org.italiangrid.voms.request.impl;
 
-
 /**
  * Default VOMSES information lookup strategy.
  * 
  * This implementation looks for vomses information in the following paths:
  * <ul>
- * 	<li>/etc/vomses
- * 	<li>${user.home}/.glite/vomses
+ * <li>/etc/vomses
+ * <li>${user.home}/.glite/vomses
+ * <li>${user.home}/.voms/vomses
  * </ul>
  * 
  * @author Andrea Ceccanti
- *
+ * 
  */
 public class DefaultVOMSESLookupStrategy extends BaseVOMSESLookupStrategy {
 
 	public static final String DEFAULT_VOMSES_DIR = "/etc/vomses";
-	
-	
+
 	public DefaultVOMSESLookupStrategy() {
-		super(new String[]{DEFAULT_VOMSES_DIR, 
-				System.getProperty("user.home")+"/.glite/vomses"});
+		super(new String[] { DEFAULT_VOMSES_DIR,
+				System.getProperty("user.home") + "/.glite/vomses",
+				System.getProperty("user.home") + "/.voms/vomses" });
 	}
 
 }
