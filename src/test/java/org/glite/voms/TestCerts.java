ecbdc5b874fcd0aa2ca6db22c068131419d20751
Daniele Andreotti <daniele.andreotti@cnaf.infn.it>
2012-12-19 09:21:48 +0100

    Updated test for proxy generation with parentheses in DN.

--- src/test/java/org/glite/voms/TestCerts.java
+++ src/test/java/org/glite/voms/TestCerts.java
@@ -1,36 +1,59 @@
 package org.glite.voms;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.cert.CRLException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+
 import junit.framework.TestCase;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.glite.voms.contact.cli.VomsProxyInitClient;
 
 public class TestCerts extends TestCase implements TestFixture {
 
 	public static final Logger log = Logger.getLogger(TestCerts.class);
 
+	private final String proxyFileName = File.separator + "tmp"
+			+ File.separator + "x509up_u_" + System.getProperty("user.name");
+
+	private final String password = "pass";
+
 	public void testProxyWithParenthesesInDN() throws CertificateException,
 			CRLException, IOException, InterruptedException {
 
+		final String voName = "test.vo";
+
+		final String fqan = "/test.vo";
+
+		String[] args = { "-usercert", dnWithParenthesisCert, "-userkey",
+				dnWithParenthesisKey, "-voms", voName + ":" + fqan,
+				"-password", password };
+
 		log.info("TestCerts.testProxyWithParenthesesInDN");
+
+		new VomsProxyInitClient(args);
+
 		PKIStore caStore = new PKIStore(trustDir, PKIStore.TYPE_CADIR, true);
 		PKIStore vomsTrustStore = new PKIStore(vomsDir, PKIStore.TYPE_VOMSDIR,
 				true);
 
 		PKIVerifier verifier = new PKIVerifier(vomsTrustStore, caStore);
 
-		X509Certificate[] proxyChain = PKIUtils
-				.loadCertificates("/tmp/x509up_u1000");
+		X509Certificate[] proxyCertificateChain = PKIUtils
+				.loadCertificates(proxyFileName);
 
-		boolean validChain = verifier.verify(proxyChain);
+		boolean validChain = verifier.verify(proxyCertificateChain);
 
 		log.info("Cert chain is valid? " + validChain);
 
 		assertTrue("Certificate validation failed", validChain);
 
 		verifier.cleanup();
+
+		FileUtils.deleteQuietly(new File(proxyFileName));
+
 	}
 }
--- src/test/java/org/glite/voms/TestFixture.java
+++ src/test/java/org/glite/voms/TestFixture.java
@@ -4,15 +4,17 @@ public interface TestFixture {
 
 	public static final String trustDir = "src/test/resources/trust-anchors";
 	public static final String noCRLsTrustDir = "src/test/resources/no-crls-trust-anchors";
-	
+
 	public static final String vomsDir = "src/test/resources/vomsdir";
 	public static final String testCert = "src/test/resources/certs/quasi_revoked.cert.pem";
 	public static final String revokedCert = "src/test/resources/certs/revoked.cert.pem";
 	public static final String validCert = "src/test/resources/certs/test0.cert.pem";
-	
+	public static final String dnWithParenthesisCert = "src/test/resources/certs/dn_with_parenthesis.cert.pem";
+	public static final String dnWithParenthesisKey = "src/test/resources/certs/dn_with_parenthesis.key.pem";
+
 	public static final String defaultCRL = "src/test/resources/crls/default-crl.pem";
 	public static final String testCertRevokedCRL = "src/test/resources/crls/cert-17-revoked-crl.pem";
 	public static final String expiredCRL = "src/test/resources/crls/expired-crl.pem";
-	
-	public static final String[] caHashes = {"d82942ab", "10b10516"};
+
+	public static final String[] caHashes = { "d82942ab", "10b10516" };
 }
