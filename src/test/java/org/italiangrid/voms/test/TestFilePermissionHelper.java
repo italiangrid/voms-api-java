fa3e523f0a57ae10cdc7754d6cce2faf2f39b646
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-09-30 16:40:24 +0200

    Reformatting.

--- src/test/java/org/italiangrid/voms/test/TestFilePermissionHelper.java
+++ src/test/java/org/italiangrid/voms/test/TestFilePermissionHelper.java
@@ -30,9 +30,8 @@ public class TestFilePermissionHelper {
 
   public static final String keyWithWrongPerms = "src/test/resources/perm-test/test1.key.pem";
   public static final String p12WithWrongPerms = "src/test/resources/perm-test/test1.p12";
-  
-  public static final String keyWith600Perms = "src/test/resources/perm-test/test2.key.pem";
 
+  public static final String keyWith600Perms = "src/test/resources/perm-test/test2.key.pem";
 
   @BeforeClass
   public static void setupPermissions() {
@@ -44,9 +43,9 @@ public class TestFilePermissionHelper {
       PosixFilePermission.ALL_PERMS);
     FilePermissionHelper.setFilePermissions(p12WithWrongPerms,
       PosixFilePermission.ALL_PERMS);
-    
+
     FilePermissionHelper.setFilePermissions(keyWith600Perms,
-    		PosixFilePermission.USER_RW);
+      PosixFilePermission.USER_RW);
 
   }
 
